6c0f499b001e45c9a7bab96cfd638a22c11ac922
mohamed <business.adacc@gmail.com>
2026-03-20 03:18:22 +0200

    It took me 51 minutes to create Project 2

---
+++ src/test/project/p1/project2/Account.java
@@ -0,0 +1,63 @@
+package test.project.p1.project2;
+
+public class Account {
+    private int accountNumber;
+    private String ownerName = "";
+    private double balance = 0;
+    private static Account accounts[] = new Account[100];
+    private static int accountCounter = 0;
+
+    Account(int accNumber,String name,double balance) {
+        this.accountNumber = accNumber;
+        this.ownerName = name;
+        this.balance = balance;
+        this.accounts[accountCounter].accountNumber = this.accountNumber;
+        this.accounts[accountCounter].balance = this.balance;
+        this.accounts[accountCounter].ownerName = this.ownerName;
+        accountCounter++;
+    }
+
+    private void updateAccountData() {
+        for (int i = 0;i<accountCounter;i++) {
+            if (this.accounts[i].accountNumber == this.accountNumber) {
+                this.accounts[i].accountNumber = this.accountNumber;
+                this.accounts[i].balance = this.balance;
+                this.accounts[i].ownerName = this.ownerName;
+                break;
+            }
+        }
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+        System.out.println("Deposit " + amount + "$ is successful");
+    }
+    public void withdraw(double amount) {
+        this.balance -= amount;
+        System.out.println("Withdraw " + amount + "$ is successful");
+    }
+    public void transfer(Account to,double amount) {
+        this.balance -= amount;
+        to.balance += amount;
+        System.out.println("Transfer " + amount + "$ from " + this.ownerName + "to " + to.ownerName + " is successful");
+    }
+    public String displayAccountInfo() {
+        return "\n------------------------" +
+                "\nAccount Info:" +
+                "\naccountNumber= " + accountNumber +
+                "\nownerName= " + ownerName +
+                "\nbalance= " + balance +
+                "\n------------------------";
+    }
+    public Account compare(Account account) {
+        if (this.balance == account.balance) {
+            System.out.println("This account is equals so i will return first account");
+            return this;
+        } else if(this.balance > account.balance) {
+            return this;
+        } else {
+            return account;
+        }
+    }
+
+}
--- src/test/project/p1/project2/project2.java
+++ src/test/project/p1/project2/project2.java
@@ -3,15 +3,7 @@ package test.project.p1.project2;
 public class project2 {
 
     public static void main(String [] args) {
-        Test o1 = new Test();
-        Test o2 = new Test();
-        o1.a = 5; o2.a = 5;
-        o1.b = 2; o1.b = 2;
-        if (o1.equals(o2))
-            System.out.println("Equal");
-        else
-            System.out.println("Not Equal");
-        System.out.println("Test");
+
     }
 
 }
