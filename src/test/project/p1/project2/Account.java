e629db00f2ea7c03bb50cdb11e82171451628198
mohamed <business.adacc@gmail.com>
2026-03-20 04:28:30 +0200

    fix sum issue

--- src/test/project/p1/project2/Account.java
+++ src/test/project/p1/project2/Account.java
@@ -1,34 +1,30 @@
 package test.project.p1.project2;
 
 public class Account {
-    static int createAccountNumber = 260;
+    private static int createAccountNumber = 260;
     private int accountNumber;
     private String ownerName = "";
     private double balance = 0;
-    private static Account accounts[] = new Account[100];
+    public static Account accounts[] = new Account[100];
     static int accountCounter = 0;
 
     Account(boolean forArray) {
 
     }
     Account(String name,double balance) {
-        this.accountNumber = createAccountNumber++;
+        this.accountNumber = ++createAccountNumber;
         this.ownerName = name;
         this.balance = balance;
         this.accounts[accountCounter] = new Account(true);
         this.accounts[accountCounter].accountNumber = this.accountNumber;
         this.accounts[accountCounter].balance = this.balance;
-        this.accounts[accountCounter].ownerName = this.ownerName;
-        accountCounter++;
-        createAccountNumber++;
+        this.accounts[accountCounter++].ownerName = this.ownerName;
     }
 
-    private void updateAccountData() {
+    private void updateBalance() {
         for (int i = 0;i<accountCounter;i++) {
             if (this.accounts[i].accountNumber == this.accountNumber) {
-                this.accounts[i].accountNumber = this.accountNumber;
                 this.accounts[i].balance = this.balance;
-                this.accounts[i].ownerName = this.ownerName;
                 break;
             }
         }
@@ -38,20 +34,23 @@ public class Account {
         this.balance += amount;
         System.out.println("Deposit " + amount + "$ is successful" +
                 "\nYour balance is: " + this.balance + "$");
+        this.updateBalance();
     }
     public void withdraw(double amount) {
         this.balance -= amount;
         System.out.println("Withdraw " + amount + "$ is successful" +
                 "\nYour balance is: " + this.balance + "$");
+        this.updateBalance();
     }
     public void transfer(Account to,double amount) {
         this.balance -= amount;
         to.balance += amount;
-        System.out.println("Transfer " + amount + "$ from " + this.ownerName + "to " + to.ownerName + " is successful");
+        System.out.println("Transfer " + amount + "$ from " + this.ownerName + " to " + to.ownerName + " is successful");
+        this.updateBalance();
     }
     public String displayAccountInfo() {
         return "\n------------------------" +
-                "\nAccount Info:" +
+                "\n" + ownerName +" Account Info:" +
                 "\naccountNumber= " + accountNumber +
                 "\nownerName= " + ownerName +
                 "\nbalance= " + balance +
@@ -68,4 +67,7 @@ public class Account {
         }
     }
 
+    public String getName() {
+        return this.ownerName;
+    }
 }
--- src/test/project/p1/project2/project2.java
+++ src/test/project/p1/project2/project2.java
@@ -4,10 +4,19 @@ public class project2 {
 
     public static void main(String [] args) {
 
-        Account a1 = new Account("Mohamed",15000d);
-        //Account a2 = new Account("Belal",25000d);
+        Account a1 = new Account("Mohamed",12000d);
+        Account a2 = new Account("Belal",24000d);
+        Account a3 = new Account("Osama",6000d);
         a1.deposit(500);
-        a1.displayAccountInfo();
-    }
+        a2.transfer(a3,1660);
+        a3.transfer(a1,250);
+        a1.deposit(5400);
+        a1.withdraw(400);
+        System.out.println("Winner is: " + a1.compare(a2).getName());
 
+        for(int i = 0;i<Account.accountCounter;i++) {
+                System.out.println(Account.accounts[i].displayAccountInfo());
+        }
+
+    }
 }
